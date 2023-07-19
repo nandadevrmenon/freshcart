1ec8afb18d152e72722b2b313829c64f9e3c4efd
Alienator55 <126163205+Alienator55@users.noreply.github.com>
2023-07-19 10:03:06 +0100

    added a dummy cart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
-import ProfilePage from "scenes/profilePage";
+import ProfilePage from "scenes/profilePage"; 
 import CheckoutPage from "scenes/checkoutPage";
 import NavBar from "scenes/navbar";
 import Footer from "scenes/footer";
 import { useSelector } from "react-redux";
 import ShopNavbar from "scenes/shopNavbar";
 import ShopHome from "scenes/shopHome";
+import UserCart from "scenes/userCart/UserCart";
 
 function App() {
   const isShop = useSelector((state) => {
@@ -24,6 +25,7 @@ function App() {
           <Route path="/profile/:userId" element={<ProfilePage />}></Route>
           <Route path="/checkout/:userId" element={<CheckoutPage />}></Route>
           <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/cart/:id" element={<UserCart />}></Route>
         </Routes>
         <Footer></Footer>
       </BrowserRouter>
---
+++ src/scenes/userCart/UserCart.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Container, List, ListItem, ListItemText, Typography } from "@mui/material";
+
+const UserCart = () => {
+    const cartItems = useSelector((state) => state.cartItems);
+    const totalPrice = useSelector((state) => state.totalPrice);
+
+    return (
+        <Container maxWidth={false} style={{ padding: "0 2rem" }}>
+            <Typography variant="h4">Your Shopping Cart</Typography>
+            <List>
+                {cartItems.map((item, index) => (
+                    <ListItem key={index}>
+                        <ListItemText
+                            primary={item.name}
+                            secondary={`Price: $${item.price}`}
+                        />
+                    </ListItem>
+                ))}
+            </List>
+            <Typography variant="h6">Total Price: ${totalPrice}</Typography>
+        </Container>
+    );
+};
+
+export default UserCart;
